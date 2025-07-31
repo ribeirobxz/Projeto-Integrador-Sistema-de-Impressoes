c26bbd30be0be9c348ec93e68c454854e3914930
JADERHD <143460747+JADERHD@users.noreply.github.com>
2025-07-30 22:07:00 -0300

    2025-07-30 chechbox no historico, splitconteiner no historico e saldo
    arrumei o listbox dos pacotes na tela de compra.

--- WinFormsApp1/Controls/ConsutarSaldos.Designer.cs
+++ WinFormsApp1/Controls/ConsutarSaldos.Designer.cs
@@ -31,6 +31,12 @@
             buttonVoltar = new Button();
             labelTitulo = new Label();
             listBoxListagem = new ListBox();
+            splitContainerInfo = new SplitContainer();
+            textBoxInfo = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainerInfo).BeginInit();
+            splitContainerInfo.Panel1.SuspendLayout();
+            splitContainerInfo.Panel2.SuspendLayout();
+            splitContainerInfo.SuspendLayout();
             SuspendLayout();
             // 
             // buttonVoltar
@@ -56,22 +62,62 @@
             // 
             // listBoxListagem
             // 
+            listBoxListagem.Dock = DockStyle.Fill;
             listBoxListagem.FormattingEnabled = true;
             listBoxListagem.ItemHeight = 15;
-            listBoxListagem.Location = new Point(20, 34);
+            listBoxListagem.Location = new Point(0, 0);
             listBoxListagem.Name = "listBoxListagem";
-            listBoxListagem.Size = new Size(760, 424);
+            listBoxListagem.Size = new Size(756, 282);
             listBoxListagem.TabIndex = 2;
+            listBoxListagem.SelectedIndexChanged += listBoxListagem_SelectedIndexChanged;
+            // 
+            // splitContainerInfo
+            // 
+            splitContainerInfo.BackColor = SystemColors.ControlLight;
+            splitContainerInfo.Location = new Point(19, 39);
+            splitContainerInfo.Name = "splitContainerInfo";
+            splitContainerInfo.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainerInfo.Panel1
+            // 
+            splitContainerInfo.Panel1.BackColor = SystemColors.Control;
+            splitContainerInfo.Panel1.Controls.Add(listBoxListagem);
+            splitContainerInfo.Panel1MinSize = 100;
+            // 
+            // splitContainerInfo.Panel2
+            // 
+            splitContainerInfo.Panel2.BackColor = SystemColors.Control;
+            splitContainerInfo.Panel2.Controls.Add(textBoxInfo);
+            splitContainerInfo.Panel2MinSize = 100;
+            splitContainerInfo.Size = new Size(756, 417);
+            splitContainerInfo.SplitterDistance = 282;
+            splitContainerInfo.TabIndex = 3;
+            // 
+            // textBoxInfo
+            // 
+            textBoxInfo.BackColor = SystemColors.Window;
+            textBoxInfo.Dock = DockStyle.Fill;
+            textBoxInfo.Location = new Point(0, 0);
+            textBoxInfo.Multiline = true;
+            textBoxInfo.Name = "textBoxInfo";
+            textBoxInfo.ReadOnly = true;
+            textBoxInfo.Size = new Size(756, 131);
+            textBoxInfo.TabIndex = 0;
             // 
             // ConsutarSaldos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(listBoxListagem);
+            Controls.Add(splitContainerInfo);
             Controls.Add(labelTitulo);
             Controls.Add(buttonVoltar);
             Name = "ConsutarSaldos";
             Size = new Size(800, 500);
+            splitContainerInfo.Panel1.ResumeLayout(false);
+            splitContainerInfo.Panel2.ResumeLayout(false);
+            splitContainerInfo.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainerInfo).EndInit();
+            splitContainerInfo.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -81,5 +127,7 @@
         private Button buttonVoltar;
         private Label labelTitulo;
         private ListBox listBoxListagem;
+        private SplitContainer splitContainerInfo;
+        private TextBox textBoxInfo;
     }
 }
